fd4f11ba3ebf2f86bb991f536af7f4d75c32b3a4
Cedric Kyle D. Belisariooo <cedricbelisario@gmail.com>
2025-03-03 23:49:10 +0800

    getting started vue js baby

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+  import HelloThesis from './components/HelloThesis.vue';
+</script>
+
+<template>
+  <HelloThesis />
+</template>
+
+<style scoped></style>
---
+++ src/components/AnotherComponents.vue
@@ -0,0 +1,6 @@
+<template>
+  <h2>This is Nested components</h2>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum nam est totam, commodi corrupti in?
+  </p>
+</template>
