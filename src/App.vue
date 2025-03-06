6856d220c45bb492c7fb517e6e18322ea6d3fd98
Cedric Kyle D. Belisariooo <cedricbelisario@gmail.com>
2025-03-06 19:15:58 +0800

    complex custom validator props

--- src/App.vue
+++ src/App.vue
@@ -6,29 +6,32 @@ const lastname = ref('Belisario');
 import staticprops from './components/props/staticprops.vue';
 import propsValidation from './components/props/propsValidation.vue';
 import arrayObjectProps from './components/props/arrayObjectProps.vue';
-
+import CustomValidator from './components/props/CustomValidator.vue';
 
 </script>
 
 <template>
-  <h1>Static Props</h1>
+  <h1 class="text-red-500 font-bold">Static Props</h1>
   <staticprops  name="John" />
   <hr>
   
-  <h1>Dynamic Props</h1>
+  <h1 class="text-blue-500 font-bold">Dynamic Props</h1>
   <dynamicprops :firstname="firstname" :lastname="lastname" />
   <button @click="firstname = 'John'" class="p-1 border m-1">Change firstname</button>
   <button @click="lastname = 'Doe'" class="p-1 border m-1">Change lastname</button>
   <hr>
 
-  <h1>Props Validation</h1>
+  <h1 class="text-green-500 font-bold">Props Validation</h1>
   <propsValidation fullname='cedric' :age='21' />
   <hr>
 
-  <h1>Complex Props</h1>
+  <h1 class="text-blue-500 font-bold">Complex Props</h1>
   <arrayObjectProps 
   :names="['cedric', 'kyle', 'belisario']" 
   :userInfo="{age: 21, email: 'cedric@gmail.com', address: ['Manabo', 'Abra']}"
   />
+  <hr>
+
+  <customValidator first-name="cedric" :age="21" password="12345678" />
 </template> 
 
---
+++ src/components/props/CustomValidator.vue
@@ -0,0 +1,26 @@
+<script setup>
+  defineProps({
+    firstName:{
+      type: String,
+      required: true,
+      validator: (value) => value.length > 3
+    },
+    age:{
+      type:Number,
+      required: true,
+      validator: (value) => value >= 20 && value <= 30 ? true : false
+    },
+    password:{
+      type: String,
+      required: true,
+      validator: (value) => value.length >= 8 ? true : false
+    }
+  })
+</script>
+
+<template>
+  <h1 class="text-red-500 font-bold">Custom Validator</h1>
+  <p>name: {{ firstName }}</p>
+  <p>age: {{ age }}</p>
+  <p>password: {{ password }}</p>
+</template>
