84976d50394e6f0cab9bdd8d3c6ea0a8935e119a
Cedric Kyle D. Belisariooo <cedricbelisario@gmail.com>
2025-03-06 18:23:18 +0800

    props validation

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,25 @@
 import { ref } from 'vue';
 import dynamicprops from './components/props/dynamicprops.vue';
 const firstname = ref('Cedirc');
-const lastname = ref('Kabongo');
+const lastname = ref('Belisario');
 import staticprops from './components/props/staticprops.vue';
+import propsValidation from './components/props/propsValidation.vue';
+
 
 </script>
 
 <template>
+  <h1>Static Props</h1>
   <staticprops  name="John" />
+  <hr>
+  
+  <h1>Dynamic Props</h1>
   <dynamicprops :firstname="firstname" :lastname="lastname" />
+  <button @click="firstname = 'John'" class="p-1 border m-1">Change firstname</button>
+  <button @click="lastname = 'Doe'" class="p-1 border m-1">Change lastname</button>
+  <hr>
+
+  <h1>Props Validation</h1>
+  <propsValidation fullname='cedric' :age='21' />
 </template> 
 
---
+++ src/components/props/propsValidation.vue
@@ -0,0 +1,26 @@
+<script setup>
+  //basic validation
+  // const props = defineProps({
+  //   fullname: String,
+  //   age: Number,
+  // })
+
+  //complex validation
+  const props = defineProps({
+    fullname:{
+      type: String,
+      required: true,
+      default: 'Mang Kepweng',
+    },
+    age:{
+      type: Number,
+      default: 0,
+    },
+  })
+</script>
+
+<template>
+    <h1>Hello {{ props.fullname }}</h1>
+    <h1>Age: {{ props.age }}</h1>
+</template>
+
