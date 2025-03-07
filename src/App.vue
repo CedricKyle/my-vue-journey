3cfb9ee1bb61256b2bbd442c2a472146e1d36c9b
Cedric Kyle D. Belisariooo <cedricbelisario@gmail.com>
2025-03-07 16:05:56 +0800

    lifecycle hooks with conditional rendering

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,16 @@
 <script setup>
-import SchoolComponent from './components/provide and inject/SchoolComponent.vue';
-import { provide } from 'vue';
+import LifeCycleHooks from './components/LifeCycleHooks.vue';
+import { ref } from 'vue';
 
-const studentInfo = {
-  name:'Cedric Kyle D. Belisario',
-  age:20,
-  course:'BSIT'
+const show = ref(true);
+
+const remove = () => {
+  show.value = false;
 }
-
-provide('schoolInfo', ['NCST', 'CIT-U', 'STI']) 
-
-const games = [
-  {
-    title: 'Space Explorer',
-    genre: 'Sci-Fi',
-    platform: 'PC',
-    releaseDate: '2023-05-20'
-  },
-  {
-    title: 'Fantasy Quest',
-    genre: 'Fantasy',
-    platform: 'PlayStation',
-    releaseDate: '2023-07-10'
-  },
-  {
-    title: 'Inception',
-    director: 'Christopher Nolan',
-    genre: 'Sci-Fi',
-    releaseDate: '2010-07-16'
-  },
-  {
-    title: 'The Shawshank Redemption',
-    director: 'Frank Darabont',
-    genre: 'Drama',
-    releaseDate: '1994-09-23'
-  }
-]
-
-provide('studentInfo', studentInfo);
-provide('games', games);
 </script>
 
 <template>
-  <SchoolComponent />
+  <LifeCycleHooks v-if="show" />
+  <button @click="remove" class="p-1 bg-red-500 m-1 text-white">Remove</button>
 </template> 
 
---
+++ src/components/LifeCycleHooks.vue
@@ -0,0 +1,67 @@
+<script setup>
+import { 
+  ref, 
+  onBeforeMount, 
+  onMounted, 
+  onBeforeUpdate, 
+  onUpdated, 
+  onBeforeUnmount, 
+  onUnmounted} from 'vue';
+
+const count = ref(0);
+
+const increment = () => {
+  count.value++;
+}
+
+onBeforeMount(() => {
+  console.log('data has about to be mount');
+})
+
+onMounted(() => {
+  console.log('data has been mounted');
+})
+
+onBeforeUpdate(() => {
+  console.log('data has about to be updated');
+})
+
+onUpdated(() => {
+  console.log('data has been updated');
+})
+
+onBeforeUnmount(() => {
+  console.log('data has about to be unmounted');
+})
+
+onUnmounted(() => {
+  console.log('data has been unmounted');
+})
+</script>
+
+<template>
+  <div>
+    <h1>Count: {{ count }}</h1>
+    <button @click="increment" class="p-1 bg-blue-500 m-1 text-white">Increment</button>
+  </div>
+</template> 
+
+<!--App.vue-->
+
+<!-- <script setup>
+import LifeCycleHooks from './components/LifeCycleHooks.vue';
+import { ref } from 'vue';
+
+const show = ref(true);
+
+const remove = () => {
+  show.value = false;
+}
+</script>
+
+<template>
+  <LifeCycleHooks v-if="show" />
+  <button @click="remove" class="p-1 bg-red-500 m-1 text-white">Remove</button>
+</template>  -->
+
+
